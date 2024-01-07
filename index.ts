36c26102ecc688cfd7282dc8c27e3c3ab64fc031
LostBitset <102832670+LostBitset@users.noreply.github.com>
2024-01-07 16:08:09 -0500

    sick

--- index.ts
+++ index.ts
@@ -1,3 +1,18 @@
 import { toHtml } from "./md_to_html";
 
-console.log(toHtml("# Yo\n$$\\otimes$$"))
+function htmlResponse(src: string): Response {
+    let html: string;
+    if (src.startsWith("<!DOCTYPE html>")) {
+        html = src;
+    } else {
+        html = `<!DOCTYPE html><html>${src}</html>`;
+    }
+    return new Response(html, {headers:{"Content-Type":"text/html"}});
+}
+
+Bun.serve({
+    fetch(request) {
+        return htmlResponse(toHtml("# Hello Bun!"));
+    },
+    port: 7400,
+});
