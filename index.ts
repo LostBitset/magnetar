2a327daea2d6aa45ee42142e590481d783d3159f
LostBitset <102832670+LostBitset@users.noreply.github.com>
2024-01-08 09:50:28 -0500

    seems alright

--- index.ts
+++ index.ts
@@ -7,12 +7,28 @@ function htmlResponse(src: string): Response {
     } else {
         html = `<!DOCTYPE html><html>${src}</html>`;
     }
-    return new Response(html, {headers:{"Content-Type":"text/html"}});
+    return new Response(html, {headers: {"Content-Type": "text/html"}});
+}
+
+function notFoundResponse(html: string): Response {
+    return new Response(html, {
+        status: 404,
+        headers: {"Content-Type": "text/html"},
+    })
 }
 
 Bun.serve({
-    fetch(request) {
-        return htmlResponse(toHtml("# Hello Bun!"));
+    fetch(req) {
+        let url = new URL(req.url);
+        if (url.pathname === "/") {
+            return notFoundResponse("1");
+        } else if (url.pathname.startsWith("/edit/")) {
+            return notFoundResponse("2");
+        } else if (url.pathname.startsWith("/view/")) {
+            return htmlResponse(toHtml("# Hello Bun!"));
+        } else {
+            return notFoundResponse(toHtml("# Page Not Found"));
+        }
     },
     port: 7400,
 });
