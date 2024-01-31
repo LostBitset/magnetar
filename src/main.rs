ad8475b9076a4eee68d296b5dd85c0799d3dc464
LostBitset <102832670+LostBitset@users.noreply.github.com>
2024-01-31 10:32:42 -0600

    warp seems to work

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let hello =
+        warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+    warp::serve(hello)
+        .run(([127, 0, 0, 1], 7404))
+        .await;
+}
